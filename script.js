66472ef253048d5142e4f902df6646ecae11a73c
infinityshades <tash.aneiq@gmail.com>
2019-11-14 06:12:46 +0100

    functioning script

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
-const url = "https://striveschool.herokuapp.com/api/product";
-const username = "user22";
-const password = "ykeZdCYNLs2dqbMc";
+window.onload = async()=> {
+   let username = "user22" 
+   let passkey = "ykeZdCYNLs2dqbMc"
+   let token = btoa( username + ":" + passkey)
 
-window.onload = async () => {
-  let response = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: "Basic " + btoa(username + ":" + password)
-    }
-  });
-  console.log(response);
-};
+   let response = await fetch("https://strive-school-testing-apis.herokuapp.com/api/product/", {
+       headers: {
+            "Authorization": "Basic " + token
+       }
+    })
+    let responseJson= await response.json ()
+    console.log(responseJson)
+}
 
-saveProduct = async event => {
-    const response = await fetch (url, {
-        method: "POST",
-        body: JSON.stringify(event),
-        headers: new Headers ({ 
-            Authorization: "Basic " + btoa(username + ":" + password),
-            "Content-Type": "application/json"
-        })
-    });
-    console.log(response)
-    return response;
-};
+newProduct = async (product) => {
+    let username = "user22"
+    let passkey = "ykeZdCYNLs2dqbMc"
+    let token = btoa(username + ":" + passkey)
 
-handleSubmit = async () => {
-    event.preventDefault();
-    const myProduct = {
-        name: document.querySelector("#name").value,
-        description: document.querySelector ("#description").value,
-        brand: document.querySelector ("#brand").value,
-        image: document.querySelector ("#imgUrl").value,
-        price: document.querySelector ("#price").value
-    };
-    console.log("myProduct", JSON.stringify(myProduct));
+    let response = await fetch ("https://strive-school-testing-apis.herokuapp.com/api/product/",{
+        method:"POST",
+        body: JSON.stringify(product),
+        headers: {
+            "Authorization":"Basic " + token,
+            "Content-type" :"application/json"
+        }
+    })
 
-    const response = await saveProduct(myProduct);
+    let responseJson= await response.json ()
 
-    if (response.ok) {
-        //checking the ok property which stores the successfull result of the operation
-        alert("the event was saved successfully");
-      } else {
-        alert("the event was NOT saved successfully");
-      }
+    // let newDiv = document.querySelector("#")
+    
+
 }
